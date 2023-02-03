4aa2d3dbf0843d2cf9025f2dacc61b46bce0455a
Park Inpum <inpum.park@medistream.co.kr>
2023-02-03 17:23:49 +0900

    node modules

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from  'express';
 import { Board } from './@core/types/types';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors())
 
 app.get('/', (req, res, next) => {
     res.send('ok');
@@ -9,7 +12,7 @@ app.get('/', (req, res, next) => {
 
 app.get('/api/article', (req, res, next) => {
   const boards: Board[] = [
-    {id: 141, content: '첫 번째 게시글 입니다.'},
+    {id: 141, content: '첫 번째 게시글 입니asdf다.'},
     {id: 145, content: '두 번째 게시글 입니다.'}
   ]
   res.send(boards);
