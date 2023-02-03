cd6c1fcec1dec894c3c21aa77b11076f93ff1030
Park Inpum <inpum.park@medistream.co.kr>
2023-02-03 16:41:42 +0900

    import type

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from  'express';
+import Board from './@core/types/types';
 
 const app = express();
 
@@ -7,10 +8,11 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/api/article', (req, res, next) => {
-  res.send([
+  const boards: Board[] = [
     {id: 141, content: '첫 번째 게시글 입니다.'},
     {id: 145, content: '두 번째 게시글 입니다.'}
-  ]);
+  ]
+  res.send(boards);
 });
 
 app.listen(3030, () => {
